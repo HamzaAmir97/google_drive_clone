5ba1cc84a071b36334025f03259beed7d39a2841
Hmazah <alhamza2012@gmail.com>
2025-05-27 12:00:00 +0300

    done with home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -59,6 +59,8 @@ const Dashboard = async () => {
         </ul>
       </section>
 
+
+
       {/* Recent files uploaded */}
       <section className="dashboard-recent-files">
         <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
